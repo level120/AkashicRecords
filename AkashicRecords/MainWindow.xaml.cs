c451c6bd3e79554949107fe4c56184cbde0169c9
Gusam Park <tnara9573@gmail.com>
2017-07-26 13:32:41 +0900

    Bug fix
    1. 318, 319번 카드 누락
    2. 미출시된 카드 초기화 누락

--- AkashicRecords/MainWindow.xaml.cs
+++ AkashicRecords/MainWindow.xaml.cs
@@ -30,11 +30,12 @@ namespace AkashicRecords
             star3 = 12,
             star4 = 10,
             star5 = 6,
-            limit = 6
-        }
-        public static int SIZE = ( int )limit.limit + ( int )limit.star2 + ( int )limit.star3 + ( int )limit.star4 + ( int )limit.star5;
+            limit = 6,
+            Unkown = 6
+        }   // 미리엄 제외, Unknown = 아직 미출시
+        public static int SIZE = ( int )limit.limit + ( int )limit.Unkown + ( int )limit.star2 + ( int )limit.star3 + ( int )limit.star4 + ( int )limit.star5;
         private int star_2 = ( int )limit.star2;
-        private int star_3 = ( int )limit.star3;  // 미리엄 제외
+        private int star_3 = ( int )limit.star3;
         private int star_4 = ( int )limit.star4;
         private int star_5 = ( int )limit.star5;
         private int price = 0;
@@ -49,14 +50,14 @@ namespace AkashicRecords
             InitializeComponent();
 
             temp = new CardList();
-            card = new List<AkashicRecords.Card>(SIZE);
+            card = new List<AkashicRecords.Card>( SIZE );
 
-            for (int i=0, n=200, k=1; i<SIZE; i++, k++ )
+            for ( int i = 0, n = 200, k = 1; i < SIZE; i++, k++ )
             {
                 if ( i == ( int )limit.star2 ) { n = 300; k = 1; }
                 else if ( i == ( int )limit.star2 + ( int )limit.star3 + ( int )limit.limit + 1 ) { n = 400; k = 1; }
-                else if ( i == ( int )limit.star2 + ( int )limit.star3 + ( int )limit.star4 + ( int )limit.limit  + 1 ) { n = 500; k = 1; }
-                card.Add( new AkashicRecords.Card( @"Images/Star/" + (n + k) + @".png", CardList.card_list[ i ].star, CardList.card_list[ i ].name ) );
+                else if ( i == ( int )limit.star2 + ( int )limit.star3 + ( int )limit.star4 + ( int )limit.limit + 1 ) { n = 500; k = 1; }
+                card.Add( new AkashicRecords.Card( @"Images/Star/" + ( n + k ) + @".png", CardList.card_list[ i ].star, CardList.card_list[ i ].name ) );
             }
         }
 
@@ -64,14 +65,14 @@ namespace AkashicRecords
         {
             string lgsText;
 
-            lgsText = tb_cost_res.Text.Replace(",", ""); // 숫자변환 시 콤마로 발생하는 에러방지
-            tb_cost_res.Text = String.Format("{0:#,###}", Convert.ToDouble(lgsText));
+            lgsText = tb_cost_res.Text.Replace( ",", "" ); // 숫자변환 시 콤마로 발생하는 에러방지
+            tb_cost_res.Text = String.Format( "{0:#,###}", Convert.ToDouble( lgsText ) );
 
             tb_cost_res.SelectionStart = tb_cost_res.MaxLength; // 캐럿을 맨 뒤로 보낸다(?)
             tb_cost_res.SelectionLength = 0;
         }
 
-        private void btn_10p_Click(object sender, RoutedEventArgs e)
+        private void btn_10p_Click( object sender, RoutedEventArgs e )
         {
             work();
         }
@@ -80,58 +81,58 @@ namespace AkashicRecords
         {
             try
             {
-                if ((Convert.ToDouble(tb_rand2.Text) + Convert.ToDouble(tb_rand3.Text)
-                    + Convert.ToDouble(tb_rand4.Text) + Convert.ToDouble(tb_rand5.Text)) != 100.0)
+                if ( ( Convert.ToDouble( tb_rand2.Text ) + Convert.ToDouble( tb_rand3.Text )
+                    + Convert.ToDouble( tb_rand4.Text ) + Convert.ToDouble( tb_rand5.Text ) ) != 100.0 )
                 {
-                    MessageBox.Show("확률값은 모두 합해 100이 되어야 합니다.", null, MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show( "확률값은 모두 합해 100이 되어야 합니다.", null, MessageBoxButton.OK, MessageBoxImage.Error );
                     return;
                 }
-                Convert.ToInt32(tb_cost.Text);
+                Convert.ToInt32( tb_cost.Text );
             }
             catch
             {
-                MessageBox.Show("TextBox 중 올바르지 않은 값이 있습니다.", null, MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show( "TextBox 중 올바르지 않은 값이 있습니다.", null, MessageBoxButton.OK, MessageBoxImage.Error );
                 return;
             }
 
-            tb_cost_res.Text = "" + (price += Convert.ToInt32(tb_cost.Text) * 10);
+            tb_cost_res.Text = "" + ( price += Convert.ToInt32( tb_cost.Text ) * 10 );
             cost();
             title_show();
 
             Random akashic = new Random();
-            select_akashic(akashic1, akashic);
-            select_akashic(akashic2, akashic);
-            select_akashic(akashic3, akashic);
-            select_akashic(akashic4, akashic);
-            select_akashic(akashic5, akashic);
-            select_akashic(akashic6, akashic);
-            select_akashic(akashic7, akashic);
-            select_akashic(akashic8, akashic);
-            select_akashic(akashic9, akashic);
-            select_akashic(akashic10, akashic);
+            select_akashic( akashic1, akashic );
+            select_akashic( akashic2, akashic );
+            select_akashic( akashic3, akashic );
+            select_akashic( akashic4, akashic );
+            select_akashic( akashic5, akashic );
+            select_akashic( akashic6, akashic );
+            select_akashic( akashic7, akashic );
+            select_akashic( akashic8, akashic );
+            select_akashic( akashic9, akashic );
+            select_akashic( akashic10, akashic );
         }
 
-        public void init_akashic(Button btn)    // 빈 카드로 초기화
+        public void init_akashic( Button btn )    // 빈 카드로 초기화
         {
             string url = "Images/0.png";
 
-            Uri resourceUri = new Uri(url, UriKind.Relative);
-            StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+            Uri resourceUri = new Uri( url, UriKind.Relative );
+            StreamResourceInfo streamInfo = Application.GetResourceStream( resourceUri );
 
-            BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+            BitmapFrame temp = BitmapFrame.Create( streamInfo.Stream );
             var brush = new ImageBrush();
             brush.ImageSource = temp;
 
             btn.Background = brush;
         }
 
-        public void select_akashic(Button btn, Random akashic)   // 아카식 뽑아 그림 채우기
+        public void select_akashic( Button btn, Random akashic )   // 아카식 뽑아 그림 채우기
         {
             string url = "Images/";
-            int star = akashic.Next(0, 9999);
-            int number = select_star(star);
+            int star = akashic.Next( 0, 9999 );
+            int number = select_star( star );
 
-            if (number < 0) return;
+            if ( number < 0 ) return;
 
             int res = select_card( number, akashic );
             url += "" + res + ".png";
@@ -142,7 +143,7 @@ namespace AkashicRecords
             {
                 index = res - 201;
             }
-            else if (number == 3 )
+            else if ( number == 3 )
             {
                 index = res + ( int )limit.star2 - 301;
             }
@@ -154,72 +155,72 @@ namespace AkashicRecords
             {
                 index = res + ( int )limit.star2 + ( int )limit.star3 + ( int )limit.star4 + ( int )limit.limit + 1 - 501;
             }
-            
+
             card[ index ].count++;
             all_count++;
 
-            Uri resourceUri = new Uri(url, UriKind.Relative);
-            StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+            Uri resourceUri = new Uri( url, UriKind.Relative );
+            StreamResourceInfo streamInfo = Application.GetResourceStream( resourceUri );
 
-            BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+            BitmapFrame temp = BitmapFrame.Create( streamInfo.Stream );
             var brush = new ImageBrush();
             brush.ImageSource = temp;
 
             btn.Background = brush;
         }
 
-        public int select_star(int res)     // 등급 뽑기
+        public int select_star( int res )     // 등급 뽑기
         {
-            double card2 = Convert.ToDouble(tb_rand2.Text) * 100;
-            double card3 = Convert.ToDouble(tb_rand3.Text) * 100;
-            double card4 = Convert.ToDouble(tb_rand4.Text) * 100;
-            double card5 = Convert.ToDouble(tb_rand5.Text) * 100;
+            double card2 = Convert.ToDouble( tb_rand2.Text ) * 100;
+            double card3 = Convert.ToDouble( tb_rand3.Text ) * 100;
+            double card4 = Convert.ToDouble( tb_rand4.Text ) * 100;
+            double card5 = Convert.ToDouble( tb_rand5.Text ) * 100;
 
-            if (res < card2)
+            if ( res < card2 )
             {
-                tb_rand2_res.Text = "" + (Convert.ToInt32(tb_rand2_res.Text) + 1);
+                tb_rand2_res.Text = "" + ( Convert.ToInt32( tb_rand2_res.Text ) + 1 );
                 return 2;
             }
-            else if (res < (card2 + card3))
+            else if ( res < ( card2 + card3 ) )
             {
-                tb_rand3_res.Text = "" + (Convert.ToInt32(tb_rand3_res.Text) + 1);
+                tb_rand3_res.Text = "" + ( Convert.ToInt32( tb_rand3_res.Text ) + 1 );
                 return 3;
             }
-            else if (res < (card2 + card3 + card4))
+            else if ( res < ( card2 + card3 + card4 ) )
             {
-                tb_rand4_res.Text = "" + (Convert.ToInt32(tb_rand4_res.Text) + 1);
+                tb_rand4_res.Text = "" + ( Convert.ToInt32( tb_rand4_res.Text ) + 1 );
                 return 4;
             }
-            else if (res < (card2 + card3 + card4 + card5))
+            else if ( res < ( card2 + card3 + card4 + card5 ) )
             {
-                tb_rand5_res.Text = "" + (Convert.ToInt32(tb_rand5_res.Text) + 1);
+                tb_rand5_res.Text = "" + ( Convert.ToInt32( tb_rand5_res.Text ) + 1 );
                 return 5;
             }
             else
                 return -1;
         }
 
-        public int select_card(int star, Random akashic)  // 카드 뽑기
+        public int select_card( int star, Random akashic )  // 카드 뽑기
         {
-            int[] limit = new int[4] { star_2, star_3, star_4, star_5 };
-            int hid = akashic.Next(0, 1000);
+            int[] limit = new int[ 4 ] { star_2, star_3, star_4, star_5 };
+            int hid = akashic.Next( 0, 1000 );
 
             int res = 0;
-            if (hid > 299)
+            if ( hid > 299 )
             {
-                while ((res = akashic.Next(1, limit[star - 2] + 1)) % 2 != 1) ;
-                if (star == 5 && !flag) this.find = true;
+                while ( ( res = akashic.Next( 1, limit[ star - 2 ] + 1 ) ) % 2 != 1 ) ;
+                if ( star == 5 && !flag ) this.find = true;
             }
             else
             {
-                while ((res = akashic.Next(1, limit[star - 2] + 1)) % 2 != 0) ;
-                if (star == 5) this.find = true;
+                while ( ( res = akashic.Next( 1, limit[ star - 2 ] + 1 ) ) % 2 != 0 ) ;
+                if ( star == 5 ) this.find = true;
             }
 
             return star * 100 + res;
         }
 
-        private void btn_reset_Click(object sender, RoutedEventArgs e)      // 리셋
+        private void btn_reset_Click( object sender, RoutedEventArgs e )      // 리셋
         {
             title_hide();
 
@@ -234,18 +235,18 @@ namespace AkashicRecords
             this.price = this.all_count = 0;
             this.find = this.flag = false;
 
-            init_akashic(akashic1);
-            init_akashic(akashic2);
-            init_akashic(akashic3);
-            init_akashic(akashic4);
-            init_akashic(akashic5);
-            init_akashic(akashic6);
-            init_akashic(akashic7);
-            init_akashic(akashic8);
-            init_akashic(akashic9);
-            init_akashic(akashic10);
+            init_akashic( akashic1 );
+            init_akashic( akashic2 );
+            init_akashic( akashic3 );
+            init_akashic( akashic4 );
+            init_akashic( akashic5 );
+            init_akashic( akashic6 );
+            init_akashic( akashic7 );
+            init_akashic( akashic8 );
+            init_akashic( akashic9 );
+            init_akashic( akashic10 );
 
-            foreach ( var i in card)
+            foreach ( var i in card )
             {
                 i.count = 0;
             }
@@ -279,7 +280,7 @@ namespace AkashicRecords
             img_card10.Visibility = Visibility.Hidden;
         }
 
-        private void btn_akashic_Click(object sender, RoutedEventArgs e)
+        private void btn_akashic_Click( object sender, RoutedEventArgs e )
         {
             akashic_work();
         }
@@ -289,7 +290,7 @@ namespace AkashicRecords
             limitakashic_work();
         }
 
-        private void btn_highakashic_Click(object sender, RoutedEventArgs e)
+        private void btn_highakashic_Click( object sender, RoutedEventArgs e )
         {
             highakashic_work();
         }
@@ -345,43 +346,43 @@ namespace AkashicRecords
             this.Title = @"아카식레코드 시뮬레이터 - 고급 아카식 모드";
         }
 
-        private void btn_test_Click(object sender, RoutedEventArgs e)
+        private void btn_test_Click( object sender, RoutedEventArgs e )
         {
             btn_10p.IsEnabled = btn_reset.IsEnabled = btn_test.IsEnabled = btn_test_hidden.IsEnabled = false;
-            while (!this.find)
+            while ( !this.find )
             {
                 work();
 
                 this.Dispatcher.Invoke(
-                    (ThreadStart)(() => {}), DispatcherPriority.ApplicationIdle);
+                    ( ThreadStart )( () => { } ), DispatcherPriority.ApplicationIdle );
 
-                Thread.Sleep(30);
+                Thread.Sleep( 30 );
 
                 this.Dispatcher.Invoke(
-                    (ThreadStart)(() => {}), DispatcherPriority.ApplicationIdle);
+                    ( ThreadStart )( () => { } ), DispatcherPriority.ApplicationIdle );
             }
-            MessageBox.Show("5성 카드 발견!", null, MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            MessageBox.Show( "5성 카드 발견!", null, MessageBoxButton.OK, MessageBoxImage.Asterisk );
             this.find = false;
             btn_10p.IsEnabled = btn_reset.IsEnabled = btn_test.IsEnabled = btn_test_hidden.IsEnabled = true;
         }
 
-        private void btn_test_hidden_Click(object sender, RoutedEventArgs e)
+        private void btn_test_hidden_Click( object sender, RoutedEventArgs e )
         {
             this.flag = true;
             btn_10p.IsEnabled = btn_reset.IsEnabled = btn_test.IsEnabled = btn_test_hidden.IsEnabled = false;
-            while (!this.find)
+            while ( !this.find )
             {
                 work();
 
                 this.Dispatcher.Invoke(
-                    (ThreadStart)(() => { }), DispatcherPriority.ApplicationIdle);
+                    ( ThreadStart )( () => { } ), DispatcherPriority.ApplicationIdle );
 
-                Thread.Sleep(30);
+                Thread.Sleep( 30 );
 
                 this.Dispatcher.Invoke(
-                    (ThreadStart)(() => { }), DispatcherPriority.ApplicationIdle);
+                    ( ThreadStart )( () => { } ), DispatcherPriority.ApplicationIdle );
             }
-            MessageBox.Show("5성 히든카드 발견!", null, MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            MessageBox.Show( "5성 히든카드 발견!", null, MessageBoxButton.OK, MessageBoxImage.Asterisk );
             this.find = this.flag = false;
             btn_10p.IsEnabled = btn_reset.IsEnabled = btn_test.IsEnabled = btn_test_hidden.IsEnabled = true;
         }
--- AkashicRecords/obj/Debug/AkashicRecords_Content.g.i.cs
+++ AkashicRecords/obj/Debug/AkashicRecords_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/maze_21_trash.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/318.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/319.png")]
 
 
